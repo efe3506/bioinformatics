e24208a571fc7802c9ebcd30f6e610db8aa90cdf
efedalli <efedalli96@gmail.com>
2023-06-27 18:00:18 +0300

    v2

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-def say_hi():
-    print("Hello pickle man")
-say_hi()
+#def say_hi():
+#   print("Hello pickle man")
+#say_hi()
+
+import datetime
+
+def say_hi_with_style():
+    print("Say hi to my little friend !")
+    print("with date and time:", datetime.datetime.now())
+
+say_hi_with_style()
